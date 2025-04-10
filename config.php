e723bc562e2dfcd87614130062fa3fd4507b07e1
Nahid44332 <nahidhossen3278@gmail.com>
2025-04-10 19:20:48 +0600

    Crud

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    $conn = new mysqli ('localhost','root', '', 'data_crud');
+    // if($conn == true){
+    //     echo "Connected Successfully!";
+    // }   
+    // else{
+    //     echo "Connected Failed";
+    // }
+?>
